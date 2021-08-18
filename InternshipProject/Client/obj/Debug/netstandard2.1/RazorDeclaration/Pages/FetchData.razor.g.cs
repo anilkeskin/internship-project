55d637b865ac9d70a451dfc91606e8d6ede3a2ac
anilkeskin <44145718+anilkeskin@users.noreply.github.com>
2021-08-18 17:24:00 +0300

    Last

--- InternshipProject/Client/obj/Debug/netstandard2.1/RazorDeclaration/Pages/FetchData.razor.g.cs
+++ InternshipProject/Client/obj/Debug/netstandard2.1/RazorDeclaration/Pages/FetchData.razor.g.cs
@@ -13,105 +13,105 @@ namespace InternshipProject.Client.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\Anil\Documents\GitHub\InternshipProject\InternshipProject\Client\_Imports.razor"
+#line 1 "C:\Users\Anil\Documents\GitHub\internship-project\InternshipProject\Client\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\Anil\Documents\GitHub\InternshipProject\InternshipProject\Client\_Imports.razor"
+#line 2 "C:\Users\Anil\Documents\GitHub\internship-project\InternshipProject\Client\_Imports.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\Anil\Documents\GitHub\InternshipProject\InternshipProject\Client\_Imports.razor"
+#line 3 "C:\Users\Anil\Documents\GitHub\internship-project\InternshipProject\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\Anil\Documents\GitHub\InternshipProject\InternshipProject\Client\_Imports.razor"
+#line 4 "C:\Users\Anil\Documents\GitHub\internship-project\InternshipProject\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\Anil\Documents\GitHub\InternshipProject\InternshipProject\Client\_Imports.razor"
+#line 5 "C:\Users\Anil\Documents\GitHub\internship-project\InternshipProject\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\Anil\Documents\GitHub\InternshipProject\InternshipProject\Client\_Imports.razor"
+#line 6 "C:\Users\Anil\Documents\GitHub\internship-project\InternshipProject\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\Anil\Documents\GitHub\InternshipProject\InternshipProject\Client\_Imports.razor"
+#line 7 "C:\Users\Anil\Documents\GitHub\internship-project\InternshipProject\Client\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\Anil\Documents\GitHub\InternshipProject\InternshipProject\Client\_Imports.razor"
+#line 8 "C:\Users\Anil\Documents\GitHub\internship-project\InternshipProject\Client\_Imports.razor"
 using InternshipProject.Client;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\Anil\Documents\GitHub\InternshipProject\InternshipProject\Client\_Imports.razor"
+#line 9 "C:\Users\Anil\Documents\GitHub\internship-project\InternshipProject\Client\_Imports.razor"
 using InternshipProject.Client.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\Anil\Documents\GitHub\InternshipProject\InternshipProject\Client\_Imports.razor"
+#line 10 "C:\Users\Anil\Documents\GitHub\internship-project\InternshipProject\Client\_Imports.razor"
 using Blazored.Modal;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "C:\Users\Anil\Documents\GitHub\InternshipProject\InternshipProject\Client\_Imports.razor"
+#line 11 "C:\Users\Anil\Documents\GitHub\internship-project\InternshipProject\Client\_Imports.razor"
 using Blazored.Modal.Services;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 12 "C:\Users\Anil\Documents\GitHub\InternshipProject\InternshipProject\Client\_Imports.razor"
+#line 12 "C:\Users\Anil\Documents\GitHub\internship-project\InternshipProject\Client\_Imports.razor"
 using InternshipProject.Client.CustomComponents.Buttons;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 13 "C:\Users\Anil\Documents\GitHub\InternshipProject\InternshipProject\Client\_Imports.razor"
+#line 13 "C:\Users\Anil\Documents\GitHub\internship-project\InternshipProject\Client\_Imports.razor"
 using InternshipProject.Client.CustomComponents.ModalComponents;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 14 "C:\Users\Anil\Documents\GitHub\InternshipProject\InternshipProject\Client\_Imports.razor"
+#line 14 "C:\Users\Anil\Documents\GitHub\internship-project\InternshipProject\Client\_Imports.razor"
 using InternshipProject.Client.Utils;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\Anil\Documents\GitHub\InternshipProject\InternshipProject\Client\Pages\FetchData.razor"
+#line 2 "C:\Users\Anil\Documents\GitHub\internship-project\InternshipProject\Client\Pages\FetchData.razor"
 using InternshipProject.Shared;
 
 #line default
@@ -126,7 +126,7 @@ using InternshipProject.Shared;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 38 "C:\Users\Anil\Documents\GitHub\InternshipProject\InternshipProject\Client\Pages\FetchData.razor"
+#line 38 "C:\Users\Anil\Documents\GitHub\internship-project\InternshipProject\Client\Pages\FetchData.razor"
        
     private WeatherForecast[] forecasts;
 
